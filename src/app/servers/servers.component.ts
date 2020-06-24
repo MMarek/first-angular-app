bd6f20c8cca92cd818264beda2f86d506b6127aa
Marek Mildner <mildnermarek@gmail.com>
2020-06-24 22:05:23 +0200

    directives: ngIf, ngStyle, ngClass, also exercise with secret password button

--- src/app/servers/servers.component.ts
+++ src/app/servers/servers.component.ts
@@ -12,6 +12,11 @@ export class ServersComponent implements OnInit {
   allowNewServer = false;
   serverCreationStatus = 'No server was created!';
   serverName = 'TestServer';
+  serverCreated = false;
+  servers = ['TestServer', 'TestServer 2'];
+  
+  showSecret = false;
+  log = [];
 
   constructor() {
     setTimeout(() => {
@@ -23,6 +28,8 @@ export class ServersComponent implements OnInit {
   }
 
   onCreateServer() {
+    this.serverCreated = true;
+    this.servers.push(this.serverName);
     this.serverCreationStatus = 'Server was created ' + this.serverName;
   }
 
@@ -30,4 +37,11 @@ export class ServersComponent implements OnInit {
     this.serverName = (<HTMLInputElement>event.target).value;
   }
 
+  onToggleDetails() {
+    this.showSecret = !this.showSecret;
+    // this.log.push(this.log.length + 1);
+    this.log.push(new Date());
+  }
+
 }
+
